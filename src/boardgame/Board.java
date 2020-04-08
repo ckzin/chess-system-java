884da3c3ffc6890fda48f4e2bdaf25ee8962e5b5
Erick Paulo <erick.pas17@gmail.com>
2020-04-08 01:56:39 -0300

    Possible moves of a piece

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -5,6 +5,7 @@ public class Board {
 	private int rows;
 	private int columns;
 	private Piece[][] pieces;
+	
 	public Board(int rows, int columns) {
 		if(rows < 1 || columns < 1) {
 			throw new BoardException("Error creating board: there must be at least 1 row and 1 column");
